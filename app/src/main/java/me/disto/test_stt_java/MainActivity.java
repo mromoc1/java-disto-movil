1057402bcd08b3fc2286986ceee20938386669e1
mromoc1 <mromo@alumnosuls.cl>
2023-01-09 20:08:27 -0300

    Init : Version lista para ser testeada

---
+++ app/src/main/java/me/disto/test_stt_java/MainActivity.java
@@ -0,0 +1,118 @@
+package me.disto.test_stt_java;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
+
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    private TextView returnedText;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
+    private final String LOG_TAG = "VoiceRecognitionActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        returnedText = findViewById(R.id.textView1);
+        resetSpeechRecognizer();
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+            return;
+        }
+
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+
+        speech.startListening(recognizerIntent);
+    }
+
+    private void resetSpeechRecognizer() {
+        if(speech != null)
+            speech.destroy();
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        if(SpeechRecognizer.isRecognitionAvailable(this))
+            speech.setRecognitionListener(this);
+        else
+            finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                speech.startListening(recognizerIntent);
+            } else {
+                Toast.makeText(MainActivity.this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {}
+    @Override
+    public void onBufferReceived(byte[] buffer) {}
+    @Override
+    public void onEndOfSpeech() {}
+
+    @Override
+    public void onResults(Bundle results) {
+        ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        StringBuilder text = new StringBuilder();
+        for (String result : matches)
+            text.append(result).append("\n");
+        Log.i(LOG_TAG, "onResults" + text.toString());
+//        returnedText.setText(text.toString());
+        speech.startListening(recognizerIntent);
+    }
+
+    @Override
+    public void onError(int errorCode) {
+        resetSpeechRecognizer();
+        speech.startListening(recognizerIntent);
+    }
+
+    @Override
+    public void onEvent(int arg0, Bundle arg1) {}
+
+    @Override
+    public void onPartialResults(Bundle results) {
+        ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        StringBuilder text = new StringBuilder();
+        for (String result : matches)
+            text.append(result).append("\n");
+        Log.i(LOG_TAG, "onPartialResults: " + text.toString());
+
+
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle arg0) {}
+
+    @Override
+    public void onRmsChanged(float rmsdB) {}
+
+}
