b37298b41037aa9cf56f969cef8332f7fc3ed215
mromoc1 <mromo@alumnosuls.cl>
2023-03-09 21:07:53 -0300

    FIX: aprendizajeDiscurso error de carga en android 11 o superior. Error persistente al finalizar la peticion http

--- app/src/main/java/me/disto/distoapp/ui/modulo_aprendizaje_discurso/AprendizajeDiscursoActivity.java
+++ app/src/main/java/me/disto/distoapp/ui/modulo_aprendizaje_discurso/AprendizajeDiscursoActivity.java
@@ -2,6 +2,8 @@ package me.disto.distoapp.ui.modulo_aprendizaje_discurso;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 
@@ -10,12 +12,15 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.IOException;
 
 import me.disto.distoapp.R;
@@ -31,6 +36,8 @@ public class AprendizajeDiscursoActivity extends BaseActivity {
     //  UI
 
     private MediaRecorder mediaRecorder;
+    private AudioRecord audioRecord;
+    private Thread recordingThread;
     private String audioFilePath;
     private Boolean estaGrabando = false;
     private String usuario = "useridx";
@@ -38,19 +45,19 @@ public class AprendizajeDiscursoActivity extends BaseActivity {
 
     private static final int PERMISSIONS_REQUEST_CODE = 100;
     private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
     private String[] preguntas = {"¿Qué te gusta hacer en tu tiempo libre?",
-                                "¿Cuál es tu opinión sobre la educación en línea?",
-                                "¿Qué planes tienes para tu futuro profesional?",
-                                "¿Qué te gustaría ser cuando seas mayor?",
-                                "¿Cuál es el lugar más interesante que has visitado y por qué?",
-                                "¿Qué aspectos te gustan de tu trabajo actual y cuáles te gustaría cambiar?",
-                                "¿¿Qué habilidades te gustaría aprender o mejorar?",
-                                "¿Como te ves en 5 años?",
-                                "¿Qué cambios positivos te gustaría ver en tu comunidad o en el mundo en general?"};
-
+            "¿Cuál es tu opinión sobre la educación en línea?",
+            "¿Qué planes tienes para tu futuro profesional?",
+            "¿Qué te gustaría ser cuando seas mayor?",
+            "¿Cuál es el lugar más interesante que has visitado y por qué?",
+            "¿Qué aspectos te gustan de tu trabajo actual y cuáles te gustaría cambiar?",
+            "¿¿Qué habilidades te gustaría aprender o mejorar?",
+            "¿Como te ves en 5 años?",
+            "¿Qué cambios positivos te gustaría ver en tu comunidad o en el mundo en general?"};
 
 
     @Override
@@ -69,31 +76,28 @@ public class AprendizajeDiscursoActivity extends BaseActivity {
         textoPregunta = findViewById(R.id.texto_pregunta);
 
 
-        if(!tienePermisos()){
+        if (!tienePermisos()) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
         }
 
-        botonIniciarDetener.setOnClickListener(new View.OnClickListener(){
+        botonIniciarDetener.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!estaGrabando){
+                if (!estaGrabando) {
                     iniciarGrabacion();
-                }else{
+                } else {
                     detenerGrabacion();
                 }
             }
         });
     }
-    private void iniciarGrabacion() {
-        File directorio = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-        String audioFileName = "record.wav";
-        File audioFile = new File(directorio, audioFileName);
-        String audioFilePath = audioFile.getAbsolutePath();
+
+    private void iniciarGrabacion(){
         mediaRecorder = new MediaRecorder();
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mediaRecorder.setOutputFile(audioFilePath);
+        mediaRecorder.setOutputFile(getExternalCacheDir().getAbsolutePath() + "/record.wav");
         try {
             mediaRecorder.prepare();
             mediaRecorder.start();
@@ -104,7 +108,6 @@ public class AprendizajeDiscursoActivity extends BaseActivity {
             textoEstado.setText("Hubo un problema al iniciar la grabación");
             throw new RuntimeException(e);
         }
-
     }
     private void detenerGrabacion() {
         mediaRecorder.stop();
@@ -112,7 +115,7 @@ public class AprendizajeDiscursoActivity extends BaseActivity {
         textoEstado.setText("Detenido");
         textoPregunta.setText("");
         estaGrabando = false;
-        File audio = new File("/storage/emulated/0/download/record.wav");
+        File audio = new File(getExternalCacheDir().getAbsolutePath() + "/record.wav");
         hiloSubirArchivo hilo = new hiloSubirArchivo(audio,usuario,contrasena);
         hilo.start();
     }
